35186b1431ca95557f1155a73cd92ee1807e0f29
kirkgio92 <148492136+kirkgio92@users.noreply.github.com>
2023-11-01 10:44:30 +0100

    Add files via upload

---
+++ 01-11-23/script.js
@@ -0,0 +1,15 @@
+const animals = [
+  "Gatto",
+  "Pinguino",
+  "Leone",
+  "Cane",
+  "Giraffa",
+  "Coccodrillo",
+  "Cavallo",
+  "Aquila",
+  "Topo",
+];
+
+for (let i = 0; i < animals.length - 1; i++) {
+  console.log(i + ": " + animals[i]);
+}
